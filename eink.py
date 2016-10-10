4080a818ad32cc151209af38345f671e00ff30ac
Doug Hall <wredgum@gmail.com>
2016-10-10 15:58:21 +0100

    first easy commands hardcoded

--- eink.py
+++ eink.py
@@ -1,10 +1,10 @@
 #
 # This is a MicroPython library for the Waveshare 4.3 e-ink display
-# Converted from the file eink.cpp released by Waveshare
+# Converted from the file epd.cpp released by Waveshare
 # Doug Hall, 2016
 #
 
-#include <Arduino.h> - not converted yet, used for the serial interface
+from machine import uart
 
 CMD_SIZE = 512
 LEADING_FRAME = 0xA5
@@ -21,11 +21,13 @@ BLACK = 0x00
 
 # commands
 CMD_HANDSHAKE = 0x00
+_cmd_handshake  = b"\xa5\x00\x09\x00\xcc\x33\xc3\x3c\xac"
 CMD_SET_BAUD = 0x01
 CMD_READ_BAUD = 0x02
 CMD_MEMORYMODE = 0x07
 CMD_STOPMODE = 0x08
 CMD_UPDATE = 0x0A
+_cmd_update     = b"\xa5\x00\x09\x0a\xcc\x33\xc3\x3c\xa6"
 CMD_SCREEN_ROTATION = 0x0D
 CMD_LOAD_FONT = 0x0E
 CMD_LOAD_PIC = 0x0F
@@ -40,6 +42,7 @@ CMD_FILL_CIRCLE = 0x27
 CMD_DRAW_TRIANGLE = 0x28
 CMD_FILL_TRIANGLE = 0x29
 CMD_CLEAR = 0x2E
+_cmd_clear      = b"\xa5\x00\x09\x2e\xcc\x33\xc3\x3c\x82"
 CMD_DRAW_STRING = 0x30
 CMD_DRAW_BITMAP = 0x70
 
@@ -60,36 +63,24 @@ EPD_NORMAL = 0
 EPD_INVERSION = 1
 
 # commands
-_cmd_handshake[8] = {0xA5, 0x00, 0x09, CMD_HANDSHAKE, 0xCC, 0x33, 0xC3, 0x3C};
-_cmd_read_baud[8] = {0xA5, 0x00, 0x09, CMD_READ_BAUD, 0xCC, 0x33, 0xC3, 0x3C};
-_cmd_stopmode[8] = {0xA5, 0x00, 0x09, CMD_STOPMODE, 0xCC, 0x33, 0xC3, 0x3C};
-_cmd_update[8] = {0xA5, 0x00, 0x09, CMD_UPDATE, 0xCC, 0x33, 0xC3, 0x3C};
-_cmd_load_font[8] = {0xA5, 0x00, 0x09, CMD_LOAD_FONT, 0xCC, 0x33, 0xC3, 0x3C};
-_cmd_load_pic[8] = {0xA5, 0x00, 0x09, CMD_LOAD_PIC, 0xCC, 0x33, 0xC3, 0x3C};
+_cmd_read_baud[8]	= {0xA5, 0x00, 0x09, CMD_READ_BAUD, 0xCC, 0x33, 0xC3, 0x3C};
+_cmd_stopmode[8]	= {0xA5, 0x00, 0x09, CMD_STOPMODE, 0xCC, 0x33, 0xC3, 0x3C};
+_cmd_update[8]		= {0xA5, 0x00, 0x09, CMD_UPDATE, 0xCC, 0x33, 0xC3, 0x3C};
+_cmd_load_font[8]	= {0xA5, 0x00, 0x09, CMD_LOAD_FONT, 0xCC, 0x33, 0xC3, 0x3C};
+_cmd_load_pic[8]	= {0xA5, 0x00, 0x09, CMD_LOAD_PIC, 0xCC, 0x33, 0xC3, 0x3C};
+
 _cmd_buff[CMD_SIZE]
 
 # pins
 wake_up = 2
 reset = 3
 
-
-static void _putchars( unsigned char * ptr, n)
+# write to display
+static void writeDisplay( unsigned char * ptr, n)
 	int i, x
 	for(i = 0; i < n; i++)
 		x = ptr[i]
 		Serial.write(x)
-
-static unsigned char _verify( void * ptr, n)
-	int i
-	unsigned char p = (unsigned char *)ptr
-	unsigned char result
-	for(i = 0, result = 0; i < n; i++)
-		result ^= p[i]
-	return result
-
-
-
-
 
 def eink_init(self, void):
 	Serial.begin(115200)
@@ -117,7 +108,7 @@ def eink_handshake(self, void):
 	memcpy(_cmd_buff, _cmd_handshake, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
 
-	_putchars(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 9)
 
 def eink_set_baud(self, baud):
 	_cmd_buff[0] = LEADING_FRAME
@@ -133,132 +124,63 @@ def eink_set_baud(self, baud):
 	_cmd_buff[10] = FRAME_E2
 	_cmd_buff[11] = FRAME_E3
 	_cmd_buff[12] = _verify(_cmd_buff, 12)
-	_putchars(_cmd_buff, 13)
+	writeDisplay(_cmd_buff, 13)
 	delay(10)
 	Serial.begin(baud)
 
 def eink_read_baud(self, void):
 	memcpy(_cmd_buff, _cmd_read_baud, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
-	_putchars(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 9)
 
-'''******************************************************************************
-* Function Name  : void eink_set_memory(unsigned char mode)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_set_memory(self, char mode):
 	_cmd_buff[0] = LEADING_FRAME
-
 	_cmd_buff[1] = 0x00
 	_cmd_buff[2] = 0x0A
-
 	_cmd_buff[3] = CMD_MEMORYMODE
-
 	_cmd_buff[4] = mode
-
 	_cmd_buff[5] = FRAME_E0
 	_cmd_buff[6] = FRAME_E1
 	_cmd_buff[7] = FRAME_E2
 	_cmd_buff[8] = FRAME_E3
 	_cmd_buff[9] = _verify(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 10)
 
-	_putchars(_cmd_buff, 10)
-
-
-'''******************************************************************************
-* Function Name  : void eink_enter_stopmode(void)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_enter_stopmode(self, void):
 	memcpy(_cmd_buff, _cmd_stopmode, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
+	writeDisplay(_cmd_buff, 9)
 
-	_putchars(_cmd_buff, 9)
-
-'''******************************************************************************
-* Function Name  : void eink_udpate(void)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_udpate(self, void):
 	memcpy(_cmd_buff, _cmd_update, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
+	writeDisplay(_cmd_buff, 9)
 
-	_putchars(_cmd_buff, 9)
-
-'''******************************************************************************
-* Function Name  : void eink_screen_rotation(unsigned char mode)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_screen_rotation(self, char mode):
 	_cmd_buff[0] = LEADING_FRAME
-
 	_cmd_buff[1] = 0x00
 	_cmd_buff[2] = 0x0A
-
 	_cmd_buff[3] = CMD_SCREEN_ROTATION
-
 	_cmd_buff[4] = mode
-
 	_cmd_buff[5] = FRAME_E0
 	_cmd_buff[6] = FRAME_E1
 	_cmd_buff[7] = FRAME_E2
 	_cmd_buff[8] = FRAME_E3
 	_cmd_buff[9] = _verify(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 10)
 
-	_putchars(_cmd_buff, 10)
-
-'''******************************************************************************
-* Function Name  : void eink_load_font(void)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_load_font(self, void):
 	memcpy(_cmd_buff, _cmd_load_font, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
 
-	_putchars(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 9)
 
-'''******************************************************************************
-* Function Name  : void eink_load_pic(void)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_load_pic(self, void):
 	memcpy(_cmd_buff, _cmd_load_pic, 8)
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
 
-	_putchars(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 9)
 
-'''******************************************************************************
-* Function Name  : void eink_set_color(unsigned char color, char bkcolor)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_set_color(self, char color, char bkcolor):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -276,16 +198,8 @@ def eink_set_color(self, char color, char bkcolor):
 	_cmd_buff[9] = FRAME_E3
 	_cmd_buff[10] = _verify(_cmd_buff, 10)
 
-	_putchars(_cmd_buff, 11)
+	writeDisplay(_cmd_buff, 11)
 
-'''******************************************************************************
-* Function Name  : void eink_set_en_font(unsigned char font)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_set_en_font(self, char font):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -302,16 +216,8 @@ def eink_set_en_font(self, char font):
 	_cmd_buff[8] = FRAME_E3
 	_cmd_buff[9] = _verify(_cmd_buff, 9)
 
-	_putchars(_cmd_buff, 10)
+	writeDisplay(_cmd_buff, 10)
 
-'''******************************************************************************
-* Function Name  : void eink_set_ch_font(unsigned char font)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_set_ch_font(self, char font):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -328,16 +234,8 @@ def eink_set_ch_font(self, char font):
 	_cmd_buff[8] = FRAME_E3
 	_cmd_buff[9] = _verify(_cmd_buff, 9)
 
-	_putchars(_cmd_buff, 10)
+	writeDisplay(_cmd_buff, 10)
 
-'''******************************************************************************
-* Function Name  : void eink_draw_pixel(int x0, y0)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_draw_pixel(self, x0, y0):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -357,16 +255,8 @@ def eink_draw_pixel(self, x0, y0):
 	_cmd_buff[11] = FRAME_E3
 	_cmd_buff[12] = _verify(_cmd_buff, 12)
 
-	_putchars(_cmd_buff, 13)
+	writeDisplay(_cmd_buff, 13)
 
-'''******************************************************************************
-* Function Name  : void eink_draw_line(int x0, y0, x1, y1)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_draw_line(self, x0, y0, x1, y1):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -390,16 +280,8 @@ def eink_draw_line(self, x0, y0, x1, y1):
 	_cmd_buff[15] = FRAME_E3
 	_cmd_buff[16] = _verify(_cmd_buff, 16)
 
-	_putchars(_cmd_buff, 17)
+	writeDisplay(_cmd_buff, 17)
 
-'''******************************************************************************
-* Function Name  : void eink_fill_rect(int x0, y0, x1, y1)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_fill_rect(self, x0, y0, x1, y1):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -423,16 +305,8 @@ def eink_fill_rect(self, x0, y0, x1, y1):
 	_cmd_buff[15] = FRAME_E3
 	_cmd_buff[16] = _verify(_cmd_buff, 16)
 
-	_putchars(_cmd_buff, 17)
+	writeDisplay(_cmd_buff, 17)
 
-'''******************************************************************************
-* Function Name  : void eink_draw_circle(int x0, y0, r)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_draw_circle(self, x0, y0, r):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -455,16 +329,8 @@ def eink_draw_circle(self, x0, y0, r):
 	_cmd_buff[13] = FRAME_E3
 	_cmd_buff[14] = _verify(_cmd_buff, 14)
 
-	_putchars(_cmd_buff, 15)
+	writeDisplay(_cmd_buff, 15)
 
-'''******************************************************************************
-* Function Name  : void eink_fill_circle(int x0, y0, r)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_fill_circle(self, x0, y0, r):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -487,16 +353,8 @@ def eink_fill_circle(self, x0, y0, r):
 	_cmd_buff[13] = FRAME_E3
 	_cmd_buff[14] = _verify(_cmd_buff, 14)
 
-	_putchars(_cmd_buff, 15)
+	writeDisplay(_cmd_buff, 15)
 
-'''******************************************************************************
-* Function Name  : void eink_draw_triangle(int x0, y0, x1, y1, x2, y2)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_draw_triangle(self, x0, y0, x1, y1, x2, y2):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -524,16 +382,8 @@ def eink_draw_triangle(self, x0, y0, x1, y1, x2, y2):
 	_cmd_buff[19] = FRAME_E3
 	_cmd_buff[20] = _verify(_cmd_buff, 20)
 
-	_putchars(_cmd_buff, 21)
+	writeDisplay(_cmd_buff, 21)
 
-'''******************************************************************************
-* Function Name  : void eink_fill_triangle(int x0, y0, x1, y1, x2, y2)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_fill_triangle(self, x0, y0, x1, y1, x2, y2):
 	_cmd_buff[0] = LEADING_FRAME
 
@@ -561,16 +411,8 @@ def eink_fill_triangle(self, x0, y0, x1, y1, x2, y2):
 	_cmd_buff[19] = FRAME_E3
 	_cmd_buff[20] = _verify(_cmd_buff, 20)
 
-	_putchars(_cmd_buff, 21)
+	writeDisplay(_cmd_buff, 21)
 
-'''******************************************************************************
-* Function Name  : void eink_clear(void)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_clear(self, void):
 	_cmd_buff[0] = LEADING_FRAME
 	_cmd_buff[1] = 0x00
@@ -581,17 +423,9 @@ def eink_clear(self, void):
 	_cmd_buff[6] = FRAME_E2
 	_cmd_buff[7] = FRAME_E3
 	_cmd_buff[8] = _verify(_cmd_buff, 8)
-	_putchars(_cmd_buff, 9)
+	writeDisplay(_cmd_buff, 9)
 
 
-'''******************************************************************************
-* Function Name  : void eink_disp_char(unsigned char ch, x0, y0)
-* Description    :
-* Input          :
-* Output         : None
-* Return         :
-* Attention		   : None
-******************************************************************************'''
 def eink_disp_char(self, char ch, x0, y0):
 	unsigned char buff[2]
 
@@ -628,7 +462,7 @@ def eink_disp_string(self, * p, x0, y0):
 	_cmd_buff[string_size + 3] = FRAME_E3
 	_cmd_buff[string_size + 4] = _verify(_cmd_buff, string_size + 4)
 
-	_putchars(_cmd_buff, string_size + 5)
+	writeDisplay(_cmd_buff, string_size + 5)
 
 def eink_disp_bitmap(self, * p, x0, y0):
 	int string_size
@@ -659,4 +493,4 @@ def eink_disp_bitmap(self, * p, x0, y0):
 	_cmd_buff[string_size + 3] = FRAME_E3
 	_cmd_buff[string_size + 4] = _verify(_cmd_buff, string_size + 4)
 
-	_putchars(_cmd_buff, string_size + 5)
+	writeDisplay(_cmd_buff, string_size + 5)
